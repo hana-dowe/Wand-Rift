1a2a704eda44b11ff65d18596bec23a0c1035ee0
hana-dowe <han_d87@outlook.com>
2023-10-26 01:00:52 -0400

    add sucky vfx to obstacles

--- Assets/FPS/Scripts/Obstacles/Vines.cs
+++ Assets/FPS/Scripts/Obstacles/Vines.cs
@@ -1,26 +1,38 @@
 using Unity.FPS.Game;
 using UnityEngine;
+using System.Collections;
 
 namespace Unity.FPS.OBSTACLES
 {
     public class Vines : MonoBehaviour
     {
+        Health m_Health;
         // public Animator Animator;
 
         [Tooltip("The random hit damage effects")]
         public ParticleSystem[] RandomHitSparks;
+
+        public GameObject Mesh;
+        public GameObject FireEffects;
 
         Destructable m_destructable;
         const string k_AnimOnDamagedParameter = "OnDamaged";
 
         void Start()
         {
+            m_Health = GetComponent<Health>();
+            m_Health.OnDie += OnDie;
+
             m_destructable = GetComponent<Destructable>();
             if (!m_destructable)
             {
                 m_destructable = GetComponentInChildren<Destructable>();
             }
 
+
+            FireEffects.SetActive(false);
+
+            m_destructable.OverrideOnDie = true;
             m_destructable.ShouldTakeDamage += ShouldTakeDamage;
         }      
 
@@ -38,9 +50,25 @@ namespace Unity.FPS.OBSTACLES
             }
 
            // Animator.SetTrigger(k_AnimOnDamagedParameter);
+           FireEffects.SetActive(true);
 
             m_destructable.TakeDamage(damage, damageSource);
 
         }
+
+        IEnumerator BurnVines()
+        {
+            yield return new WaitForSeconds(0.3f);
+            Mesh.SetActive(false);
+            yield return new WaitForSeconds(0.2f);
+            Destroy(gameObject);
+        }
+
+        void OnDie()
+        {
+            StartCoroutine(BurnVines());
+        }
+
+
     }
 }
